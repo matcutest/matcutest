3de72341f0b7a94f0b256f1a8c6aac025d6b53cd
zaikunzhang <zaikunzhang@gmail.com>
2023-01-01 00:33:26 +0800

    evise `chcup`

--- mtools/msrc/chcup.m
+++ mtools/msrc/chcup.m
@@ -23,19 +23,18 @@ for ip = minip : np
     pname = plist{ip};
     fprintf("%d. %s\n", ip, pname);
 
-   try
-        p = macup(pname);
-        for ir = 1 : nr
-            x0 = p.x0;
-            r = randn(size(x0));
-            r = r / norm(r);
-            x = x0 + 0.1 * norm(x0)*r;
-            y = p.objective(x);
-            if ~isempty(p.nonlcon)
-                [cineq, ceq] = p.nonlcon(x);
-            end
-        end
-    catch
-        blacklist = [blacklist, {pname}];
-    end
+    try
+         p = macup(pname);
+         for ir = 1 : nr
+             x0 = p.x0;
+             x = x0 + 0.5 * norm(x0) * randn(size(x0));
+             p.objective(x);
+             if ~isempty(p.nonlcon)
+                 p.nonlcon(x);
+             end
+         end
+     catch
+         blacklist = [blacklist, {pname}];
+     end
+
 end
